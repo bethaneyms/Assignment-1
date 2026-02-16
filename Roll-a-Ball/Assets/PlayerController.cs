82a5df9c7257d2cefdd9723feb80b2cc31637de1
bethaneyms <bethaneym03@gmail.com>
2026-02-16 14:39:35 -0600

    added game dev folder
    Added the game dev folder from the unity tutorial

--- Roll-a-Ball/Assets/PlayerController.cs
+++ Roll-a-Ball/Assets/PlayerController.cs
@@ -1,42 +1,38 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     public float speed = 10f;
     public float jumpForce = 15f;
-    private Rigidbody rb;
 
-    void Start()
-    {
-        // This connects the script to the Rigidbody component
+    private Rigidbody rb;
+    private AudioSource audioSource;
+
+    void Start() {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    void Update()
-    {
-        // Makes the Player jump
-        if (Input.GetKeyDown(KeyCode.Space))
-        { 
+    void Update() {
+        // Jump logic is best in Update to catch the button press instantly
+        if (Input.GetKeyDown(KeyCode.Space)) { 
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 
-    // Detects when the Player overlaps with the coin
-    void OnTriggerEnter(Collider other) 
-    {
-        // IMPORTANT: Make sure your coin tag is exactly "coin" in Unity!
-        if (other.gameObject.CompareTag("coin"))
-        {
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("coin")) {
             other.gameObject.SetActive(false);
+            
+            // Optional: play sound if audioSource is attached
+            if(audioSource != null) audioSource.Play();
         }
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement * speed);
     }
-}
+} 
